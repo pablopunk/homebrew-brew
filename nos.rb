31d5b8a6f57d07160567944809224639e68df56a
Pablo P Varela <pablo@pablopunk.com>
2024-10-10 00:34:14 +0200

    nos@0.0.4

--- nos.rb
+++ nos.rb
@@ -1,8 +1,8 @@
 class Nos < Formula
   desc "Manage your dotfiles and their dependencies automagically"
   homepage "https://github.com/pablopunk/nos"
-  url "https://github.com/pablopunk/nos/archive/refs/tags/0.0.3.tar.gz"
-  sha256 "d9fc1b80ddaf05fcaa0c6c9c475088cff8ec759bccfaf37437c7de707b23fca5"
+  url "https://github.com/pablopunk/nos/archive/refs/tags/0.0.4.tar.gz"
+  sha256 "f9e071224a0d0c9acd19758deacc6a46279465a3933e0ed2c205ec385e2ae384"
   license "MIT"
 
   depends_on "lua"
