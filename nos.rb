d1e611835d2f48537446c6a9ffc986c3c98b1930
Pablo P Varela <pablo@pablopunk.com>
2024-10-10 13:25:02 +0200

    reuse version in nos

--- nos.rb
+++ nos.rb
@@ -1,8 +1,10 @@
 class Nos < Formula
+  version "0.0.5"
+  sha256 "87f5ce9361f8ef1263521ff9b12ca39df00f32a1486753220e55278a11bc3129"
+
   desc "Manage your dotfiles and their dependencies automagically"
   homepage "https://github.com/pablopunk/nos"
-  url "https://github.com/pablopunk/nos/archive/refs/tags/0.0.5.tar.gz"
-  sha256 "87f5ce9361f8ef1263521ff9b12ca39df00f32a1486753220e55278a11bc3129"
+  url "https://github.com/pablopunk/nos/archive/refs/tags/#{version}.tar.gz"
   license "MIT"
 
   depends_on "lua"
