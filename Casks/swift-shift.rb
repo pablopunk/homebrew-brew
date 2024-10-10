152b90dea5ae8108e9898e2ed7600fcc888305c2
Pablo P Varela <pablo@pablopunk.com>
2024-10-10 13:24:26 +0200

    reuse version

--- Casks/swift-shift.rb
+++ Casks/swift-shift.rb
@@ -2,12 +2,12 @@ cask "swift-shift" do
   version "0.25.1"
   sha256 "87ce43be0d7614a16c089cd84c42c5981f6445579b112322bd0940554d587e03"
 
-  url "https://github.com/pablopunk/SwiftShift/releases/download/0.25.1/SwiftShift.zip"
+  url "https://github.com/pablopunk/SwiftShift/releases/download/#{version}/SwiftShift.zip"
   name "Swift Shift"
   desc "Sweet window management for macOS"
   homepage "https://swiftshift.app"
 
-  app "SwiftShift.app"
+  app "Swift Shift.app"
 
   zap trash: [
     "/Applications/SwiftShift.app",
