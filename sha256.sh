77e43aabf0d840428c6462b12c95b9eb175833ad
Pablo P Varela <pablo@pablopunk.com>
2024-10-10 13:10:06 +0200

    swift-shift

--- sha256.sh
+++ sha256.sh
@@ -6,8 +6,13 @@ if [ -z "$version" ]; then
     exit 1
 fi
 
-curl -sL -o $package-$version.tar.gz https://github.com/pablopunk/$package/archive/refs/tags/$version.tar.gz
-
-sha256sum $package-$version.tar.gz
-
-rm $package-$version.tar.gz
+if [ "$package" == "swifthi-shift" ]; then
+    curl -sL -o SwiftShift.zip https://github.com/pablopunk/$package/releases/download/$version/$package.zip
+    sha256sum SwiftShift.zip
+    rm SwiftShift.zip
+    exit 0
+else
+    curl -sL -o $package-$version.tar.gz https://github.com/pablopunk/$package/archive/refs/tags/$version.tar.gz
+    sha256sum $package-$version.tar.gz
+    rm $package-$version.tar.gz
+fi
