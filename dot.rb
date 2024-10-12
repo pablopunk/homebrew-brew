b7b095f8bfd85c89662d394829fbd2f7085f7a6a
Pablo P Varela <pablo@pablopunk.com>
2024-10-12 14:22:00 +0200

    nos is now dot

--- dot.rb
+++ dot.rb
@@ -1,19 +1,19 @@
-class Nos < Formula
+class Dot < Formula
   version "0.0.8"
   sha256 "3899787657baa807808b09dec67ae240ad4ecc71e21880fbcf3c40a3ae9afa25"
 
   desc "Manage your dotfiles and their dependencies automagically"
-  homepage "https://github.com/pablopunk/nos"
-  url "https://github.com/pablopunk/nos/archive/refs/tags/#{version}.tar.gz"
+  homepage "https://github.com/pablopunk/dot"
+  url "https://github.com/pablopunk/dot/archive/refs/tags/#{version}.tar.gz"
   license "MIT"
 
   depends_on "lua"
 
   def install
-    bin.install "nos.lua" => "nos"
+    bin.install "dot.lua" => "dot"
   end
 
   test do
-    system "#{bin}/nos", "--version"
+    system "#{bin}/dot", "--version"
   end
 end
