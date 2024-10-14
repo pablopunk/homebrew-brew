62b3d78266e9a401ef21d61647802e0a499a48c2
Pablo P Varela <pablo@pablopunk.com>
2024-10-14 10:08:44 +0200

    odf

---
+++ odf.rb
@@ -0,0 +1,17 @@
+class Odf < Formula
+  version "2.3.3"
+  sha256 "398cd0025a76037a68eec8f54d62c8418c1d59f5113c928b4db5695f0f1afbb9"
+
+  desc "Open Diff Files. Open git-modified files in your favorite editor."
+  homepage "https://github.com/pablopunk/odf"
+  url "https://github.com/pablopunk/odf/archive/refs/tags/#{version}.tar.gz"
+  license "MIT"
+
+  def install
+    bin.install "odf" => "odf"
+  end
+
+  test do
+    system "#{bin}/odf", "--version"
+  end
+end
