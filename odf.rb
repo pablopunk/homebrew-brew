590ed83572f845efb7fa04a4c4c1f7d8ecd6ba37
Pablo P Varela <pablo@pablopunk.com>
2025-01-21 16:27:02 +0100

    odf@3.0.2

--- odf.rb
+++ odf.rb
@@ -1,6 +1,6 @@
 class Odf < Formula
-  version "3.0.1"
-  sha256 "7e9be8424f9013581bb784109d1c418e866b886974c007ce30cab4b3db7ec9f6"
+  version "3.0.2"
+  sha256 "5cc5b38e52829458c5ccf345afbb73fb5c0b579c23f235942900f708e9cd5811"
 
   desc "Open Diff Files. Open git-modified files in your favorite editor."
   homepage "https://github.com/pablopunk/odf"
