2c73d01f9cacb81da0970912646513baf82241bc
Pablo P Varela <pablo@pablopunk.com>
2025-05-03 03:21:57 +0200

    dot@0.7.0

--- dot.rb
+++ dot.rb
@@ -1,6 +1,6 @@
 class Dot < Formula
-  version "0.6.0"
-  sha256 "2be87028a16d9f493f29618b52687075530d2caef72a1daec630c17a49f851da"
+  version "0.7.0"
+  sha256 "26ba9a217afcfc75f2652b8db295f268364f9be994533537d715fed3af152773"
 
   desc "Manage your dotfiles and their dependencies automagically"
   homepage "https://github.com/pablopunk/dot"
