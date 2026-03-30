97426a34d20579c4679b240d38ae5f6785b80837
Pablo P Varela <pablo@pablopunk.com>
2026-03-30 12:30:27 +0200

    edges: add versioned formula v0.1.0

--- edges.rb
+++ edges.rb
@@ -2,7 +2,10 @@ class Edges < Formula
   desc "A lightweight window border tool for macOS"
   homepage "https://github.com/pablopunk/edges"
   license "MIT"
+  url "https://github.com/pablopunk/edges/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "e98b19b297c1dfac083e632bde6a0961cf308c7a9f1cc2e03ac7fea21ba1708c"
   head "https://github.com/pablopunk/edges.git", branch: "main"
+  version "0.1.0"
 
   depends_on "rust" => :build
   depends_on :macos
