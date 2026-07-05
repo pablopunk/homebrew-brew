892e47ddc08e529cf3e73e794ac33ab98ca6b832
Pablo P Varela <pablo@pablopunk.com>
2026-07-05 17:48:59 +0200

    dot v4.0.3

--- dot.rb
+++ dot.rb
@@ -1,28 +1,28 @@
 class Dot < Formula
   desc "Manage your dotfiles and their dependencies automagically"
   homepage "https://github.com/pablopunk/dot"
-  version "4.0.2"
+  version "4.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/pablopunk/dot/releases/download/v#{version}/dot-darwin-x64"
-      sha256 "ac5757525c25fc90585a9aea040d6915ada4f333ce9050e975b7eab3066b555a"
+      sha256 "cd972cb0364edadffe2a363f7ddb512df7d2193e291e38c98c2c753f6f65b154"
     end
     if Hardware::CPU.arm?
       url "https://github.com/pablopunk/dot/releases/download/v#{version}/dot-darwin-arm64"
-      sha256 "226eb2e4ccb992d19f0f80a4fe8bad6f985cf36acad097d2bd7e6fe6e1e065e7"
+      sha256 "313aef538f10235fcf9fbd03743bfe20a7d867011190a7de394048a93f5760a2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/pablopunk/dot/releases/download/v#{version}/dot-linux-x64"
-      sha256 "d581b2bfa087c9d3804ad6516bbaff449a58cdef07f357b925ae071af837a8f0"
+      sha256 "954584d8ff9056d6335051a1bb9f76693f243499ecf7567ddb28d46dc2010907"
     end
     if Hardware::CPU.arm?
       url "https://github.com/pablopunk/dot/releases/download/v#{version}/dot-linux-arm64"
-      sha256 "b5c52aa43307d93fa00c48225e1e859ab150e899a55fccb35e48f19e96516c68"
+      sha256 "760b97df35b27913d67bd9addf2f96bee2204cee0858bcc5cb1ad8ab908ed502"
     end
   end
 
